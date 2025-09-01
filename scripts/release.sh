af141129cade824246518d4c398740e5f50d022b
Batkhuu Byambajav <batkhuu10@gmail.com>
2025-09-02 01:16:24 +0900

    :hammer: Fix formatting and add missing exclamation mark in error message in release script; add blank line for readability in backup script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
+
 ## --- Base --- ##
 # Getting path of this script file:
 _SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
@@ -19,7 +20,7 @@ if [ -z "$(which gh)" ]; then
 fi
 
 if ! gh auth status >/dev/null 2>&1; then
-	echo "[ERROR]: You need to login: 'gh auth login'"
+	echo "[ERROR]: You need to login: 'gh auth login'!"
 	exit 1
 fi
 ## --- Base --- ##
